e44999c9f20f3e03720d7d8de91464ce5f1d60e1
Donald Kapyouo <kapygenius@gmail.com>
2022-05-27 17:23:13 +0200

    initial kapyshop

--- src/main/java/com/example/kapyedu/KapyshopApplication.java
+++ src/main/java/com/example/kapyedu/KapyshopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KapyeduApplication {
+public class KapyshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KapyeduApplication.class, args);
+		SpringApplication.run(KapyshopApplication.class, args);
 	}
 
 }
